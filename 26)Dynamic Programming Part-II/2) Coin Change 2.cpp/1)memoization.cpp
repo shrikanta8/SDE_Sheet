5dda5a2d54dcd63054f7900ba5c802ffb9b51555
Shriji <42779746+shrikanta8@users.noreply.github.com>
2022-06-16 07:04:06 +0530

    Create 1)memoization.cpp

---
+++ 26)Dynamic Programming Part-II/2) Coin Change 2.cpp/1)memoization.cpp
@@ -0,0 +1,29 @@
+ minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+
+        if(ind == 0){
+            if(T%arr[0] == 0) return T/arr[0];
+            else return 1e9;
+        }
+
+        if(dp[ind][T]!=-1)
+            return dp[ind][T];
+
+        int notTaken = 0 + minimumElementsUtil(arr,ind-1,T,dp);
+
+        int taken = 1e9;
+        if(arr[ind] <= T)
+            taken = 1 + minimumElementsUtil(arr,ind,T-arr[ind],dp);
+
+        return dp[ind][T] = min(notTaken,taken);
+    }
+
+
+    int coinChange(vector<int>& arr, int T){
+
+        int n= arr.size();
+
+        vector<vector<int>> dp(n,vector<int>(T+1,-1));
+        int ans =  minimumElementsUtil(arr, n-1, T, dp);
+        if(ans >= 1e9) return -1;
+        return ans;
+    }
