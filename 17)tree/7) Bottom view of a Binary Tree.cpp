4424138e706e1ce8f165f8ea99455e39dddfc7e6
Shriji <42779746+shrikanta8@users.noreply.github.com>
2022-06-16 07:01:46 +0530

    Create 7) Bottom view of a Binary Tree.cpp

---
+++ 17)tree/7) Bottom view of a Binary Tree.cpp
@@ -0,0 +1,39 @@
+// https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+
+vector <int> bottomView(Node *root)
+{
+    vector <int> v;
+    if(!root)
+        return v ;
+    
+    queue<pair<Node* , int>> q;
+    
+    map<int,int>mp;
+    q.push({root,0});
+    Node* temp=NULL;
+    
+    while(q.size()){
+        temp=q.front().first;
+        int num = q.front().second;
+        q.pop();
+        mp[num] = temp->data;
+        if(temp->left)
+            q.push({temp->left,num-1});
+        if(temp->right)
+            q.push({temp->right,num+1});
+    }
+    for(auto i : mp)
+        v.push_back(i.second);
+    return v;
+}
+
+
+
+//Time Complexity: O(N)
+
+Space Complexity: O(N)
+
+
+
+//we can't do it recursively
+//for recursive approach we have to keep height value also
