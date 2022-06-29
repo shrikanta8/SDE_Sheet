5c6a9b8734a2946ac182e09e87da92863f2243c1
Shriji <42779746+shrikanta8@users.noreply.github.com>
2022-06-29 23:27:57 +0530

    Update 7) Find the inorder predecessor_successor of a given Key in BST.cpp

--- 20) Binary Search Tree/7) Find the inorder predecessor_successor of a given Key in BST.cpp
+++ 20) Binary Search Tree/7) Find the inorder predecessor_successor of a given Key in BST.cpp
@@ -1,25 +1,45 @@
- key=0;
+
+/* BST Node
+struct Node
+{
+	int key;
+	struct Node *left, *right;
+};
+*/
+
 // This function finds predecessor and successor of key in BST.
 // It sets pre and suc as predecessor and successor respectively
-void findPreSuc(Node* root, Node*& pre, Node*& suc, int key);
 
- void insert(struct Node *root,int n1,int n2,char lr)
- {
-     if(root==NULL)
-        return;
-     if(root->key==n1)
-     {
-         switch(lr)
-         {
-          case 'L': root->left=new Node(n2);
-                    break;
-          case 'R': root->right=new Node(n2);
-                    break;
-         }
-     }
-     else
-     {
-         insert(root->left,n1,n2,lr);
-         insert(root->right,n1,n2,lr);
-     }
- }
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int k)
+{
+    Node *root1= root;
+    pre=NULL,suc =NULL;
+    
+    //finding successor
+    while(root1!=NULL){
+        
+        if(k <= root1->key){
+            root1=root1->left;
+        }
+        else{
+            pre = root1 ;   //it can be our answer
+            root1 = root1 -> right;
+        }
+    }
+    
+    //finding predecessor
+    while(root!=NULL){
+        
+        if(k >= root->key){
+            root=root->right;
+        }
+        else{
+            suc = root;  //it can be our answer
+            root=root->left;
+        }
+    }
+    
+
+// Your code goes here
+
+}
