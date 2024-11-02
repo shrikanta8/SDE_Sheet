2812ca57222b958594f14b34190aba55a74e5882
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2024-11-02 19:06:39 +0530

    Update 4) Rod Cutting.cpp

--- 26)Dynamic Programming Part-II/4) Rod Cutting.cpp
+++ 26)Dynamic Programming Part-II/4) Rod Cutting.cpp
@@ -1,6 +1,5 @@
 //https://practice.geeksforgeeks.org/problems/rod-cutting0840/1
 
-  
   int maxValue(int price[], int len, vector<vector<int> >& dp, int ind){
         
         if(ind==0){
@@ -23,6 +22,27 @@
     int cutRod(int price[], int n) {
         vector<vector<int> > dp(n+1,vector<int>(n+1,-1));
         return maxValue(price,n,dp,n);
-        
-        //code here
+    }
+
+
+
+
+Tabulation
+int cutRod(int price[], int n) {
+        vector<vector<int> > dp(n,vector<int>(n+1));
+        for(int i=1;i<=n;i++){
+            dp[0][i]=i*price[0];
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int len=1;len<=n;len++){
+                int notTake= dp[ind-1][len];
+                int take = 0;
+                
+                if(ind + 1 <=len){
+                    take = price[ind] + dp[ind][len-(ind+1)];
+                }
+                dp[ind][len] = max(take,notTake);
+            }
+        }
+        return dp[n-1][n];
     }
