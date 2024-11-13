6a8fd7a792609bb2c5bbfc50baf3a904dc19cef3
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2024-11-14 00:25:21 +0530

    Update and rename 2) Minimum Platforms to 2) Minimum Platforms.cpp

--- 8) Greedy Algorithm/2) Minimum Platforms.cpp
+++ 8) Greedy Algorithm/2) Minimum Platforms.cpp
@@ -23,10 +23,30 @@ int findPlatform(int arr[], int dep[], int n)
                 plat_needed--;
                 j++;
             }
-    
             if (plat_needed > result)
                 result = plat_needed;
         }
     
         return result;
+    }
+
+
+
+
+- brute force way:
+  
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        vector<int> dp(2361);
+        int n=arr.size(),ans=0;
+        
+        for(int i=0;i<n;i++){
+            dp[arr[i]]++;
+            dp[dep[i]+1]--;
+        }
+        ans=dp[0];
+        for(int i=1;i<2361;i++){
+            dp[i]+=dp[i-1];
+            ans=max(ans,dp[i]);
+        }
+        return ans;        // Your code here
     }
