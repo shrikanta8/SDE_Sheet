3288028b50d5d95137d5f8a6323c70d2b2fb0b1b
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2024-12-06 17:35:59 +0530

    Update and rename 3) K-th largest element in a stream. to 3) K-th largest element in a stream..cpp

--- 22) Binary Trees[Miscellaneous]/3) K-th largest element in a stream..cpp
+++ 22) Binary Trees[Miscellaneous]/3) K-th largest element in a stream..cpp
@@ -6,9 +6,9 @@ public:
     int k1;
     KthLargest(int k, vector<int>& nums) {
         
-        int n = nums.size(),j=0;
+        int n = nums.size();
         k1=k;
-        for(int i=0;i<k1 && j<n;i++,j++){
+        for(int i=0;i<k && i<n;i++){
             pq.push(nums[i]);
         }
         for(int i=k;i<nums.size(); i++){
@@ -28,7 +28,6 @@ public:
             pq.push(val);pq.pop();
             
         }
-                           
         return pq.top();
     }
 };
